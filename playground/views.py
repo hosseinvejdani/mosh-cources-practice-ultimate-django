110c3e1c8b112a9c05451dc20c53b19fe1e64d8c
hosseinvejdani <vejdani.shojae@gmail.com>
2021-09-13 15:33:57 +0430

    end of 5.10 - limiting results

--- playground/views.py
+++ playground/views.py
@@ -4,32 +4,11 @@ from store.models import Product
 
 def say_hello(request):
 
+    # find first 5 products
+    products = Product.objects.all()[:5]
 
-    # # sort products by title in ASC order and render first 5 of them
-    # products = Product.objects.order_by('title')[:5]
+    # find second 5 products
+    products = Product.objects.all()[5:10]
 
-    # # sort products by title in DSC order and render first 5 of them
-    # products = Product.objects.order_by('-title')[:5]
-
-    # # sort products by title in DSC order then reverse its direction and render first 5 of them
-    # products = Product.objects.order_by('-title').reverse()[:5]
-    
-    # # sort products by unit_price in ASC oreder and then sort them by 
-    # # title in DSC order and render first 10 of them
-    # products = Product.objects.order_by('unit_price','-title')[:10]
-
-    # # find cheapest product in collection 3
-    # products = Product.objects.filter(collection__id=3).order_by('unit_price')[0]
-
-    # # find 5 cheapest products in collection 3
-    # products = Product.objects.filter(collection__id=3).order_by('unit_price')[:5]
-
-    # # find cheapest product in collection 3
-    # products = Product.objects.filter(collection__id=3).earliest('unit_price')
-
-    # find most expensive product in collection 3
-    products = Product.objects.filter(collection__id=3).latest('unit_price')
-
-
-    return render(request, 'hello.html', {'name': 'Hossein'})
+    return render(request, 'hello.html', {'name': 'Hossein', 'products':products})
 
