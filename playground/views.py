ec849e392045ffb2d60ad6cd0494ee74333bd329
hosseinvejdani <vejdani.shojae@gmail.com>
2021-09-13 11:40:17 +0430

    end of 5.5

--- playground/views.py
+++ playground/views.py
@@ -1,11 +1,21 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.core.exceptions import ObjectDoesNotExist
 from store.models import Product
 
 
 def say_hello(request):
-    query_set = Product.objects.all()
-    for product in query_set:
-        print(product)
+    # # method 1
+    # try:
+    #     product = Product.objects.filter(pk=0)
+    # except ObjectDoesNotExist:
+    #     pass
+
+    # # method 2
+    # product = Product.objects.filter(pk=1).first()
+
+    # method 3
+    exist = Product.objects.filter(pk=1).exists()
+
     return render(request, 'hello.html', {'name': 'Mosh'})
 
