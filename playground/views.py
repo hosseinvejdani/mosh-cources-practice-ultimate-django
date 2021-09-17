65da8e0eb065443bec1b499e069b4157022a29b9
hosseinvejdani <vejdani.shojae@gmail.com>
2021-09-17 11:41:43 +0430

    5.16 - calling database functions by Func class associate with annotate method

--- playground/views.py
+++ playground/views.py
@@ -1,30 +1,27 @@
-from django.db.models import Value, F
+from django.db.models import Value, F, Func
 from django.shortcuts import render
-from django.db.models.aggregates import Count, Min, Max, Avg, Sum
+from django.db.models.functions import Concat 
 from store.models import Customer, Order, OrderItem, Product
 
 
 def say_hello(request):
 
-    #  Annotate method : Annotates each object in the
-    #  QuerySet with the provided list of query expressions.
-    #  An expression may be a simple value, a reference to a
-    #  field on the model (or any related models), or an aggregate
-    #  expression (averages, sums, etc.) that has been computed
-    #  over the objects that are related to the objects in the QuerySet.
+    # annotate new field called full_name in Customer model by 
+    # calling database Func, 'CONCAT' in you query
+    # method 1
+    queryset = Customer.objects.annotate(
+        full_name = Func(F('first_name'),Value(' '),F('last_name'),function='CONCAT')
+    )
 
-    # prcatice 1 :
-    # add new field to customer called is_new with defalut True value
-    # in this case you should use Value() method
-    queryset = Customer.objects.annotate(is_new=Value(True))
-    # you can use it multiple time :
-    queryset = Customer.objects.annotate(is_new=Value(True),is_old=Value(False))
+    # method 2 
+    queryset = Customer.objects.annotate(
+        full_name = Concat('first_name',Value(' '),'last_name')
+    )
 
-    # practice 2 :
-    # add new attribute to orderitem with annotate method
-    # which calculate and shows total price as new field -> 'total_price'
-    queryset = OrderItem.objects.annotate(total_price = F('unit_price')*F('quantity'))
-    # as you can see, you can perform computional task here in  annotate method using F methods
+    # you can find more extra django built-in database functions
+    # for handling diffrent datatypes in you queries:
+    # https://docs.djangoproject.com/en/3.2/ref/models/database-functions/
+
 
     return render(request, 'hello.html', {'name': 'Hossein', 'result': list(queryset)})
 
