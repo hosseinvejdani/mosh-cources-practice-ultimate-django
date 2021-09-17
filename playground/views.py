2dc97be483bf4340ef2a632e72b8460baf9c0f7c
hosseinvejdani <vejdani.shojae@gmail.com>
2021-09-17 13:07:48 +0430

    5.18 - working with expression wrappers

--- playground/views.py
+++ playground/views.py
@@ -1,4 +1,5 @@
-from django.db.models import Value, F, Func, Count
+from django.db.models import Value, F, Func, Count,ExpressionWrapper
+from django.db.models.fields import DecimalField
 from django.shortcuts import render
 from django.db.models.functions import Concat 
 from store.models import Customer, Order, OrderItem, Product
@@ -6,15 +7,12 @@ from store.models import Customer, Order, OrderItem, Product
 
 def say_hello(request):
 
-    # add new field to customer that shows total order that each 
-    # customer has been placed
-    queryset = Customer.objects.annotate(
-        order_count = Count('order')
-    )
-
-    # add new field to product that shows total numbers that this product exist in orders
+    # by expressionswrapper you can build complex expressions:
+    # for example you can annotate new field to product called
+    # discounted_price like this:
+    discounted_price = ExpressionWrapper(F('unit_price') * 0.8,output_field=DecimalField())
     queryset = Product.objects.annotate(
-        orderitem_count = Count('orderitem')
+        discounted_price = discounted_price
     )
 
     return render(request, 'hello.html', {'name': 'Hossein', 'result': list(queryset)})
