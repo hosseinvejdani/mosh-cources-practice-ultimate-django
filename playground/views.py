fad6e4203d7a35198ae6e07696e0286061fc6c00
hosseinvejdani <vejdani.shojae@gmail.com>
2021-09-17 13:00:30 +0430

    5.17 - grouping data

--- playground/views.py
+++ playground/views.py
@@ -1,4 +1,4 @@
-from django.db.models import Value, F, Func
+from django.db.models import Value, F, Func, Count
 from django.shortcuts import render
 from django.db.models.functions import Concat 
 from store.models import Customer, Order, OrderItem, Product
@@ -6,22 +6,16 @@ from store.models import Customer, Order, OrderItem, Product
 
 def say_hello(request):
 
-    # annotate new field called full_name in Customer model by 
-    # calling database Func, 'CONCAT' in you query
-    # method 1
+    # add new field to customer that shows total order that each 
+    # customer has been placed
     queryset = Customer.objects.annotate(
-        full_name = Func(F('first_name'),Value(' '),F('last_name'),function='CONCAT')
+        order_count = Count('order')
     )
 
-    # method 2 
-    queryset = Customer.objects.annotate(
-        full_name = Concat('first_name',Value(' '),'last_name')
+    # add new field to product that shows total numbers that this product exist in orders
+    queryset = Product.objects.annotate(
+        orderitem_count = Count('orderitem')
     )
-
-    # you can find more extra django built-in database functions
-    # for handling diffrent datatypes in you queries:
-    # https://docs.djangoproject.com/en/3.2/ref/models/database-functions/
-
 
     return render(request, 'hello.html', {'name': 'Hossein', 'result': list(queryset)})
 
